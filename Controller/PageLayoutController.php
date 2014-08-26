a94fa152928aee8a2e7b0312c7c44047054cd1cf
Edi Modrić <edi.modric@gmail.com>
2014-08-26 18:02:00 +0200

    Update twig templates

--- Controller/PageLayoutController.php
+++ Controller/PageLayoutController.php
@@ -2,7 +2,7 @@
 
 namespace Netgen\Bundle\MoreBundle\Controller;
 
-use eZ\Publish\Core\MVC\Symfony\Controller\Controller;
+use eZ\Bundle\EzPublishCoreBundle\Controller;
 use eZ\Publish\API\Repository\Values\Content\Query;
 use eZ\Publish\API\Repository\Values\Content\Query\SortClause;
 use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
@@ -22,16 +22,82 @@ class PageLayoutController extends Controller
         $response = new Response();
         $response->setPublic()->setSharedMaxAge( 86400 );
 
-        $pageLayout = $this->container->get( 'netgen_more.component.page_layout' );
+        $pageLayout = $this->get( 'netgen_more.component.page_layout' );
 
         return $this->render(
             'NetgenMoreBundle::page_header.html.twig',
             array(
                 'mainCategoryLocationId' => $mainCategoryLocationId,
                 'rootLocation' => $pageLayout->getRootLocation(),
-                'siteInfoLocation' => $pageLayout->getSiteInfoLocation()
+                'siteInfoLocation' => $pageLayout->getSiteInfoLocation(),
+                'configResolver' => $this->getConfigResolver()
             ),
             $response
+        );
+    }
+
+    /**
+     * Displays path for a given $locationId
+     *
+     * @param mixed $locationId
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function path( $locationId )
+    {
+        $pathArray = array();
+
+        $locationService = $this->getRepository()->getLocationService();
+        $path = $locationService->loadLocation( $locationId )->path;
+
+        // The root location can be defined at site access level
+        $rootLocationId = $this->getConfigResolver()->getParameter( 'content.tree_root.location_id' );
+
+        /** @var \eZ\Publish\Core\Helper\TranslationHelper $translationHelper */
+        $translationHelper = $this->get( 'ezpublish.translation_helper' );
+
+        $isRootLocation = false;
+
+        // Shift of location "1" from path as it is not a fully valid location and not readable by most users
+        array_shift( $path );
+
+        for ( $i = 0; $i < count( $path ); $i++ )
+        {
+            $location = $locationService->loadLocation( $path[$i] );
+            // if root location hasn't been found yet
+            if ( !$isRootLocation )
+            {
+                // If we reach the root location, we begin to add item to the path array from it
+                if ( $location->id == $rootLocationId )
+                {
+                    $isRootLocation = true;
+                    $pathArray[] = array(
+                        'text' => $translationHelper->getTranslatedContentNameByContentInfo( $location->contentInfo ),
+                        'url' => $location->id != $locationId ? $this->generateUrl( $location ) : false
+                    );
+                }
+            }
+            // The root location has already been reached, so we can add items to the path array
+            else
+            {
+                $pathArray[] = array(
+                    'text' => $translationHelper->getTranslatedContentNameByContentInfo( $location->contentInfo ),
+                    'url' => $location->id != $locationId ? $this->generateUrl( $location ) : false
+                );
+            }
+        }
+
+        // We don't want the path to be displayed if we are on the frontpage
+        // which means we display it only if we have several items in it
+        if ( count( $pathArray ) <= 1 )
+        {
+            return new Response();
+        }
+
+        return $this->render(
+            'NetgenMoreBundle::page_path.html.twig',
+            array(
+                'pathArray' => $pathArray
+            )
         );
     }
 
@@ -47,14 +113,15 @@ class PageLayoutController extends Controller
         $response = new Response();
         $response->setPublic()->setSharedMaxAge( 86400 );
 
-        $pageLayout = $this->container->get( 'netgen_more.component.page_layout' );
+        $pageLayout = $this->get( 'netgen_more.component.page_layout' );
 
         return $this->render(
             'NetgenMoreBundle::page_footer.html.twig',
             array(
                 'mainCategoryLocationId' => $mainCategoryLocationId,
                 'rootLocation' => $pageLayout->getRootLocation(),
-                'siteInfoLocation' => $pageLayout->getSiteInfoLocation()
+                'siteInfoLocation' => $pageLayout->getSiteInfoLocation(),
+                'configResolver' => $this->getConfigResolver()
             ),
             $response
         );
@@ -63,44 +130,38 @@ class PageLayoutController extends Controller
     /**
      * Returns rendered region template
      *
-     * @param int $layoutContentId
+     * @param mixed $layoutId
      * @param string $region
-     * @param bool $cssClass
+     * @param string|bool $cssClass
      * @param array $params
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function region( $layoutContentId, $region, $cssClass = false, $params = array() )
+    public function region( $layoutId, $region, $cssClass = false, $params = array() )
     {
         $response = new Response();
         $response->setPublic()->setSharedMaxAge( 300 );
 
-        $repository = $this->getRepository();
-        $configResolver = $this->getConfigResolver();
-
-        $layoutContent = $repository->getContentService()->loadContent( $layoutContentId );
+        $layout = $this->getRepository()->getContentService()->loadContent( $layoutId );
 
         /** @var $pageValue \eZ\Publish\Core\FieldType\Page\Value */
-        $pageValue = $layoutContent->getFieldValue( 'page' );
+        $pageValue = $layout->getFieldValue( 'page' );
 
         foreach ( $pageValue->page->zones as $zone )
         {
-            if ( strcasecmp( $zone->identifier, $region ) == 0 )
+            if ( strtolower( $zone->identifier ) == strtolower( $region ) && !empty( $zone->blocks ) )
             {
-                if ( count( $zone->blocks ) > 0 )
-                {
-                    return $this->render(
-                        'NetgenMoreBundle::layout_region.html.twig',
-                        array(
-                            'layoutRegionZone' => $zone,
-                            'cssClass' => $cssClass,
-                            'configResolver' => $configResolver,
-                            'region' => $region,
-                            'params' => $params
-                        ),
-                        $response
-                    );
-                }
+                return $this->render(
+                    'NetgenMoreBundle:parts:layout_region.html.twig',
+                    array(
+                        'zone' => $zone,
+                        'region' => $region,
+                        'cssClass' => $cssClass,
+                        'params' => $params,
+                        'configResolver' => $this->getConfigResolver()
+                    ),
+                    $response
+                );
             }
         }
 

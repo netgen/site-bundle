c259b7cd9cabb856a116ad495508e22a2faaeabd
Edi Modrić <edi.modric@gmail.com>
2014-10-09 14:30:39 +0200

    Add script handler for clearing legacy autoloads

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -25,4 +25,23 @@ class ScriptHandler extends DistributionBundleScriptHandler
 
         static::executeCommand( $event, $appDir, 'ngmore:legacy:symlink', $options['process-timeout'] );
     }
+
+    /**
+     * Generates legacy autoloads
+     *
+     * @param $event \Composer\Script\CommandEvent
+     */
+    public static function generateLegacyAutoloads( CommandEvent $event )
+    {
+        $options = self::getOptions( $event );
+        $appDir = $options['symfony-app-dir'];
+
+        if ( !is_dir( $appDir ) )
+        {
+            echo 'The symfony-app-dir (' . $appDir . ') specified in composer.json was not found in ' . getcwd() . ', can not generate legacy autoloads.' . PHP_EOL;
+            return;
+        }
+
+        static::executeCommand( $event, $appDir, 'ezpublish:legacy:script bin/php/ezpgenerateautoloads.php', $options['process-timeout'] );
+    }
 }

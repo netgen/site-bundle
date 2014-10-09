cf7cc729b475850b021b688439fb485505fe264e
Edi Modrić <edi.modric@gmail.com>
2014-10-09 13:01:05 +0200

    Add composer script handler

---
+++ Composer/ScriptHandler.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Netgen\Bundle\MoreBundle\Composer;
+
+use Sensio\Bundle\DistributionBundle\Composer\ScriptHandler as DistributionBundleScriptHandler;
+use Composer\Script\CommandEvent;
+
+class ScriptHandler extends DistributionBundleScriptHandler
+{
+    /**
+     * Symlinks legacy siteaccesses and various other legacy files to their proper locations
+     *
+     * @param $event \Composer\Script\CommandEvent
+     */
+    public static function installLegacySymlinks( CommandEvent $event )
+    {
+        $options = self::getOptions( $event );
+        $appDir = $options['symfony-app-dir'];
+
+        if ( !is_dir( $appDir ) )
+        {
+            echo 'The symfony-app-dir (' . $appDir . ') specified in composer.json was not found in ' . getcwd() . ', can not install legacy symlinks.' . PHP_EOL;
+            return;
+        }
+
+        static::executeCommand( $event, $appDir, 'ngmore:legacy:symlink', $options['process-timeout'] );
+    }
+}

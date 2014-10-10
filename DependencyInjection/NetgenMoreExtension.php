15014b10649c123adea1c25886cf308ddb287bc4
Edi Modrić <edi.modric@gmail.com>
2014-10-11 00:05:16 +0200

    Allow implicit loading of bundle configuration

--- DependencyInjection/NetgenMoreExtension.php
+++ DependencyInjection/NetgenMoreExtension.php
@@ -2,17 +2,15 @@
 
 namespace Netgen\Bundle\MoreBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Yaml\Yaml;
 
-/**
- * This is the class that loads and manages your bundle configuration
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
- */
-class NetgenMoreExtension extends Extension
+class NetgenMoreExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritDoc}
@@ -28,5 +26,25 @@ class NetgenMoreExtension extends Extension
         $loader->load( 'fieldtypes.yml' );
         $loader->load( 'roles.yml' );
         $loader->load( 'services.yml' );
+    }
+
+    /**
+     * Allow an extension to prepend the extension configurations
+     *
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     */
+    public function prepend( ContainerBuilder $container )
+    {
+        $configFiles = array(
+            __DIR__ . '/../Resources/config/ezpublish.yml',
+            __DIR__ . '/../Resources/config/ezpage.yml'
+        );
+
+        foreach ( $configFiles as $configFile )
+        {
+            $config = Yaml::parse( file_get_contents( $configFile ) );
+            $container->prependExtensionConfig( 'ezpublish', $config );
+            $container->addResource( new FileResource( $configFile ) );
+        }
     }
 }

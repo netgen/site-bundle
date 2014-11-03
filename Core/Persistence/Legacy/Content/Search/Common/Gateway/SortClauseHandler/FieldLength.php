3379df59783bd3d9fd3b79f1da4c3f2f2e3febfc
Edi Modrić <edi.modric@gmail.com>
2014-11-03 14:54:52 +0100

    Remove comments

--- Core/Persistence/Legacy/Content/Search/Common/Gateway/SortClauseHandler/FieldLength.php
+++ Core/Persistence/Legacy/Content/Search/Common/Gateway/SortClauseHandler/FieldLength.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * File containing a DoctrineDatabase sort clause handler class
- *
- * @copyright Copyright (C) eZ Systems AS. All rights reserved.
- * @license For full copyright and license information view LICENSE file distributed with this source code.
- * @version 2014.07.0
- */
 
 namespace Netgen\Bundle\MoreBundle\Core\Persistence\Legacy\Content\Search\Common\Gateway\SortClauseHandler;
 

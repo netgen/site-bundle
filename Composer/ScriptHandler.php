651f2e3069ca92e948f6723546cb312746a3cc65
Edi Modrić <edi.modric@gmail.com>
2015-02-24 14:45:06 +0100

    Remove legacy symlink and autoload generator composer script handlers

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -4,29 +4,9 @@ namespace Netgen\Bundle\MoreBundle\Composer;
 
 use Sensio\Bundle\DistributionBundle\Composer\ScriptHandler as DistributionBundleScriptHandler;
 use Composer\Script\CommandEvent;
-use Symfony\Component\Process\ProcessBuilder;
 
 class ScriptHandler extends DistributionBundleScriptHandler
 {
-    /**
-     * Symlinks legacy siteaccesses and various other legacy files to their proper locations
-     *
-     * @param $event \Composer\Script\CommandEvent
-     */
-    public static function installLegacySymlinks( CommandEvent $event )
-    {
-        $options = self::getOptions( $event );
-        $appDir = $options['symfony-app-dir'];
-
-        if ( !is_dir( $appDir ) )
-        {
-            echo 'The symfony-app-dir (' . $appDir . ') specified in composer.json was not found in ' . getcwd() . ', can not install legacy symlinks.' . PHP_EOL;
-            return;
-        }
-
-        static::executeCommand( $event, $appDir, 'ngmore:symlink:legacy', $options['process-timeout'] );
-    }
-
     /**
      * Symlinks various project files and folders to their proper locations
      *
@@ -44,45 +24,5 @@ class ScriptHandler extends DistributionBundleScriptHandler
         }
 
         static::executeCommand( $event, $appDir, 'ngmore:symlink:project', $options['process-timeout'] );
-    }
-
-    /**
-     * Generates legacy autoloads
-     *
-     * @param $event \Composer\Script\CommandEvent
-     */
-    public static function generateLegacyAutoloads( CommandEvent $event )
-    {
-        $options = self::getOptions( $event );
-
-        $currentWorkingDirectory = getcwd();
-        $legacyRootDir = $currentWorkingDirectory . '/' . $options['ezpublish-legacy-dir'];
-
-        if ( !is_dir( $legacyRootDir ) )
-        {
-            echo 'The ezpublish-legacy-dir (' . $options['ezpublish-legacy-dir'] . ') specified in composer.json was not found in ' . $currentWorkingDirectory . ', can not generate legacy autoloads.' . PHP_EOL;
-            return;
-        }
-
-        chdir( $legacyRootDir );
-
-        $processBuilder = new ProcessBuilder(
-            array(
-                'php',
-                'bin/php/ezpgenerateautoloads.php'
-            )
-        );
-
-        $process = $processBuilder->getProcess();
-
-        $process->setTimeout( 3600 );
-        $process->run(
-            function ( $type, $buffer )
-            {
-                echo $buffer;
-            }
-        );
-
-        chdir( $currentWorkingDirectory );
     }
 }

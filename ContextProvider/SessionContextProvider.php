4ca4b6c456677de6bb6de37b7e5c5fae0947e500
Edi Modrić <edi.modric@gmail.com>
2015-09-16 11:59:23 +0200

    Add session and user aware user context providers

---
+++ ContextProvider/SessionContextProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Netgen\Bundle\MoreBundle\ContextProvider;
+
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use FOS\HttpCache\UserContext\ContextProviderInterface;
+use FOS\HttpCache\UserContext\UserContext;
+
+class SessionContextProvider implements ContextProviderInterface
+{
+    /**
+     * @var \Symfony\Component\HttpFoundation\Session\SessionInterface
+     */
+    protected $session;
+
+    /**
+     * Constructor
+     *
+     * @param \Symfony\Component\HttpFoundation\Session\SessionInterface $session
+     */
+    public function __construct( SessionInterface $session )
+    {
+        $this->session = $session;
+    }
+
+    /**
+     * This function is called before generating the hash of a UserContext.
+     *
+     * This allow to add a parameter on UserContext or set the whole array of parameters
+     *
+     * @param \FOS\HttpCache\UserContext\UserContext $context
+     */
+    public function updateUserContext( UserContext $context )
+    {
+        if ( $this->session->isStarted() )
+        {
+            $context->addParameter( 'sessionId', $this->session->getId() );
+        }
+    }
+}

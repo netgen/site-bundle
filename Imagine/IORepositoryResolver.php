7e848ab584c526a30525473fb916fcca221448b2
Edi Modrić <edi.modric@gmail.com>
2015-11-02 10:18:21 +0100

    Add override for Imagine repository resolver

---
+++ Imagine/IORepositoryResolver.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Netgen\Bundle\MoreBundle\Imagine;
+
+use eZ\Bundle\EzPublishCoreBundle\Imagine\IORepositoryResolver as BaseIORepositoryResolver;
+
+class IORepositoryResolver extends BaseIORepositoryResolver
+{
+    /**
+     * Returns empty string to disable absolute image URLs.
+     *
+     * Temporary solution until https://github.com/ezsystems/ezpublish-kernel/pull/1137 is merged.
+     *
+     * @return string
+     */
+    protected function getBaseUrl()
+    {
+        parent::getBaseUrl();
+    }
+}

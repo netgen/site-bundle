9deea2fd447e072ec3e7b2000d1b9216c09e76e1
Ivan Herak <ivan@netgen.hr>
2015-11-10 14:01:49 +0100

    Moved comment to kernel.yml

--- Core/Persistence/Legacy/Content/UrlAlias/Handler.php
+++ Core/Persistence/Legacy/Content/UrlAlias/Handler.php
@@ -4,11 +4,6 @@ namespace Netgen\Bundle\MoreBundle\Core\Persistence\Legacy\Content\UrlAlias;
 
 use eZ\Publish\Core\Persistence\Legacy\Content\UrlAlias\Handler as BaseHandler;
 
-/**
- * Override to enable mulitbyte url alias to work correctly
- * (fixed in EZP v6)
- *
- */
 class Handler extends BaseHandler
 {
     /**

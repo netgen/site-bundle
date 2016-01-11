35b7b87dc36d0f6cefa6e3d44b7dacaa29e530e2
Edi Modrić <edi.modric@gmail.com>
2016-01-11 14:21:29 +0100

    Add compiler pass to remove default eZ image aliases

---
+++ DependencyInjection/Compiler/RemoveDefaultImageAliasesPass.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Netgen\Bundle\MoreBundle\DependencyInjection\Compiler;
+
+use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\ConfigResolver;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class RemoveDefaultImageAliasesPass implements CompilerPassInterface
+{
+    /**
+     * Compiler pass to remove default eZ Publish image aliases.
+     *
+     * It removes image alias called reference, as well as all image aliases which
+     * reference points to "reference" image alias.
+     *
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     */
+    public function process( ContainerBuilder $container )
+    {
+        $scopes = array_merge(
+            array( ConfigResolver::SCOPE_DEFAULT ),
+            $container->getParameter( 'ezpublish.siteaccess.list' )
+        );
+
+        // Adding ezxml_tags.xsl to all scopes
+        foreach ( $scopes as $scope )
+        {
+            if ( !$container->hasParameter( "ezsettings.$scope.image_variations" ) )
+            {
+                continue;
+            }
+
+            $imageVariations = $container->getParameter( "ezsettings.$scope.image_variations" );
+
+            unset($imageVariations["reference"]);
+            foreach ( $imageVariations as $variationName => $variation )
+            {
+                if ( $variation["reference"] == "reference" )
+                {
+                    unset( $imageVariations[$variationName] );
+                }
+            }
+
+            $container->setParameter( "ezsettings.$scope.image_variations", $imageVariations );
+        }
+    }
+}

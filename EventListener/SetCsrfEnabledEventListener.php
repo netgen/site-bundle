a6fb8f74558d0a6095ac5fd8d50404f2ee5f01ce
Edi Modrić <edi.modric@gmail.com>
2016-02-19 14:39:11 +0100

    Add event listener to add csrf_enabled variable to request

---
+++ EventListener/SetCsrfEnabledEventListener.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Netgen\Bundle\MoreBundle\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Csrf\CsrfTokenManager;
+
+class SetCsrfEnabledEventListener implements EventSubscriberInterface
+{
+    /**
+     * @var \Symfony\Component\Security\Csrf\CsrfTokenManager
+     */
+    protected $csrfTokenManager;
+
+    /**
+     * Constructor.
+     *
+     * @param \Symfony\Component\Security\Csrf\CsrfTokenManager $csrfTokenManager
+     */
+    public function __construct(CsrfTokenManager $csrfTokenManager = null)
+    {
+        $this->csrfTokenManager = $csrfTokenManager;
+    }
+
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(KernelEvents::REQUEST => 'onKernelRequest');
+    }
+
+    /**
+     * Sets the variable into request indicating if CSRF protection is enabled or not.
+     *
+     * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $event->getRequest()->attributes->set(
+            'csrf_enabled',
+            $this->csrfTokenManager instanceof CsrfTokenManager
+        );
+    }
+}

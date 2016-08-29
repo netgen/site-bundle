fb24e1dbffa0779195d7429a176c5f7bae882938
Edi Modrić <edi.modric@gmail.com>
2016-08-29 20:37:46 +0200

    Rewrite download controller to use site API

--- Controller/DownloadController.php
+++ Controller/DownloadController.php
@@ -5,14 +5,57 @@ namespace Netgen\Bundle\MoreBundle\Controller;
 use eZ\Bundle\EzPublishCoreBundle\Controller;
 use eZ\Publish\Core\FieldType\BinaryBase\Value as BinaryBaseValue;
 use eZ\Publish\Core\FieldType\Image\Value as ImageValue;
-use eZ\Publish\API\Repository\Values\Content\Field;
 use eZ\Bundle\EzPublishIOBundle\BinaryStreamResponse;
+use eZ\Publish\Core\IO\IOServiceInterface;
+use Netgen\EzPlatformSite\API\LoadService;
+use Symfony\Component\Translation\TranslatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DownloadController extends Controller
 {
+    /**
+     * @var \Netgen\EzPlatformSite\API\LoadService
+     */
+    protected $loadService;
+
+    /**
+     * @var \eZ\Publish\Core\IO\IOServiceInterface
+     */
+    protected $ioFileService;
+
+    /**
+     * @var \eZ\Publish\Core\IO\IOServiceInterface
+     */
+    protected $ioImageService;
+
+    /**
+     * @var \Symfony\Component\Translation\TranslatorInterface
+     */
+    protected $translator;
+
+    /**
+     * Constructor.
+     *
+     * @param \Netgen\EzPlatformSite\API\LoadService $loadService
+     * @param \eZ\Publish\Core\IO\IOServiceInterface $ioFileService
+     * @param \eZ\Publish\Core\IO\IOServiceInterface $ioImageService
+     * @param \Symfony\Component\Translation\TranslatorInterface $translator
+     */
+    public function __construct(
+        LoadService $loadService,
+        IOServiceInterface $ioFileService,
+        IOServiceInterface $ioImageService,
+        TranslatorInterface $translator
+    )
+    {
+        $this->loadService = $loadService;
+        $this->ioFileService = $ioFileService;
+        $this->ioImageService = $ioImageService;
+        $this->translator = $translator;
+    }
+
     /**
      * Downloads the binary file specified by content ID and field ID.
      *
@@ -23,56 +66,35 @@ class DownloadController extends Controller
      * @param mixed $fieldId
      * @param bool $isInline
      *
+     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException If file or image does not exist
+     *
      * @return \eZ\Bundle\EzPublishIOBundle\BinaryStreamResponse
      */
     public function downloadFile(Request $request, $contentId, $fieldId, $isInline = false)
     {
-        $content = $this->getRepository()->getContentService()->loadContent(
+        $content = $this->loadService->loadContent(
             $contentId,
-            null,
-            $request->query->has('version') ? $request->query->get('version') : null
+            $request->query->has('version') ? $request->query->get('version') : null,
+            $request->query->has('inLanguage') ? $request->query->get('inLanguage') : null
         );
 
-        $binaryField = null;
-        foreach ($content->getFields() as $field) {
-            if ($field->id == $fieldId) {
-                $binaryField = $field;
-                break;
-            }
-        }
-
-        if (
-            !$binaryField instanceof Field ||
-            $this->container->get('ezpublish.field_helper')->isFieldEmpty(
-                $content,
-                $binaryField->fieldDefIdentifier,
-                $request->query->has('inLanguage') ? $request->query->get('inLanguage') : null
-            )
-        ) {
+        if (!$content->hasFieldById($fieldId) || $content->getFieldById($fieldId)->isEmpty()) {
             throw new NotFoundHttpException(
-                $this->container->get('translator')->trans(
-                    'ngmore.download.file_not_found'
-                )
+                $this->translator->trans('ngmore.download.file_not_found')
             );
         }
 
-        $binaryFieldValue = $this->container->get('ezpublish.translation_helper')->getTranslatedField(
-            $content,
-            $binaryField->fieldDefIdentifier,
-            $request->query->has('inLanguage') ? $request->query->get('inLanguage') : null
-        )->value;
+        $binaryFieldValue = $content->getFieldById($fieldId)->value;
 
         if ($binaryFieldValue instanceof BinaryBaseValue) {
-            $ioService = $this->container->get('ezpublish.fieldtype.ezbinaryfile.io_service');
-            $binaryFile = $ioService->loadBinaryFile($binaryFieldValue->id);
+            $ioService = $this->ioFileService;
+            $binaryFile = $this->ioFileService->loadBinaryFile($binaryFieldValue->id);
         } elseif ($binaryFieldValue instanceof ImageValue) {
-            $ioService = $this->container->get('ezpublish.fieldtype.ezimage.io_service');
-            $binaryFile = $ioService->loadBinaryFile($binaryFieldValue->id);
+            $ioService = $this->ioImageService;
+            $binaryFile = $this->ioImageService->loadBinaryFile($binaryFieldValue->id);
         } else {
             throw new NotFoundHttpException(
-                $this->container->get('translator')->trans(
-                    'ngmore.download.file_not_found'
-                )
+                $this->translator->trans('ngmore.download.file_not_found')
             );
         }
 

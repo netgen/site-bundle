7cb96d9a773a6334ddf45a0fce9cd8f333d65b0a
Edi Modrić <edi.modric@gmail.com>
2017-02-20 10:42:17 +0100

    Add cache settings for page layout controller

--- Controller/PageLayoutController.php
+++ Controller/PageLayoutController.php
@@ -5,6 +5,7 @@ namespace Netgen\Bundle\MoreBundle\Controller;
 use Knp\Menu\Provider\MenuProviderInterface;
 use Knp\Menu\Renderer\RendererProviderInterface;
 use Netgen\Bundle\EzPlatformSiteApiBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Knp\Menu\ItemInterface;
 
@@ -35,33 +36,34 @@ class PageLayoutController extends Controller
     /**
      * Returns rendered menu.
      *
+     * @param \Symfony\Component\HttpFoundation\Request $request
      * @param string $menuName
      * @param mixed $activeItemId
-     * @param string $ulClass
-     * @param string $firstClass
-     * @param string $currentClass
-     * @param string $lastClass
-     * @param string $template
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function menu($menuName, $activeItemId, $ulClass = 'nav navbar-nav', $firstClass = 'firstli', $currentClass = 'active', $lastClass = 'lastli', $template = null)
+    public function menu(Request $request, $menuName, $activeItemId)
     {
+        $requestAttributes = $request->attributes;
+
         $menu = $this->menuProvider->get($menuName);
-        $menu->setChildrenAttribute('class', $ulClass);
+        $menu->setChildrenAttribute(
+            'class',
+            $requestAttributes->get('ulClass') ?: 'nav navbar-nav'
+        );
 
         if (!empty($menu[$activeItemId]) && $menu[$activeItemId] instanceof ItemInterface) {
             $menu[$activeItemId]->setCurrent(true);
         }
 
         $menuOptions = array(
-            'firstClass' => $firstClass,
-            'currentClass' => $currentClass,
-            'lastClass' => $lastClass,
+            'firstClass' => $requestAttributes->get('firstClass') ?: 'firstli',
+            'currentClass' => $requestAttributes->get('currentClass') ?: 'active',
+            'lastClass' => $requestAttributes->get('lastClass') ?: 'lastli',
         );
 
-        if ($template !== null) {
-            $menuOptions['template'] = $template;
+        if ($requestAttributes->has('template')) {
+            $menuOptions['template'] = $requestAttributes->get('template');
         }
 
         $menuContent = $this->menuRenderer->get()->render(
@@ -76,6 +78,8 @@ class PageLayoutController extends Controller
             $response->headers->set('X-Location-Id', $menuLocationId);
         }
 
+        $this->processCacheSettings($request, $response);
+
         $response->setContent($menuContent);
 
         return $response;
@@ -84,41 +88,74 @@ class PageLayoutController extends Controller
     /**
      * Returns rendered region template.
      *
+     * @param \Symfony\Component\HttpFoundation\Request $request
      * @param mixed $layoutId
      * @param string $region
-     * @param string|bool $cssClass
-     * @param array $params
-     * @param array $blockSpecificParams
-     * @param string $template
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function region($layoutId, $region, $cssClass = false, $params = array(), $blockSpecificParams = array(), $template = null)
+    public function region(Request $request, $layoutId, $region)
     {
+        $requestAttributes = $request->attributes;
+
         $response = new Response();
         $layout = $this->getSite()->getLoadService()->loadContent($layoutId);
 
         /** @var $pageValue \eZ\Publish\Core\FieldType\Page\Value */
         $pageValue = $layout->getField('page')->value;
 
+        $template = 'NetgenMoreBundle:parts:layout_region.html.twig';
+        if ($requestAttributes->has('template')) {
+            $template = $requestAttributes->get('template');
+        }
+
+        $this->processCacheSettings($request, $response);
+        $response->headers->set('X-Location-Id', $layout->contentInfo->mainLocationId);
+
         foreach ($pageValue->page->zones as $zone) {
             if (strtolower($zone->identifier) == strtolower($region) && !empty($zone->blocks)) {
                 return $this->render(
-                    $template !== null ? $template : 'NetgenMoreBundle:parts:layout_region.html.twig',
+                    $template,
                     array(
                         'zone' => $zone,
                         'region' => $region,
-                        'css_class' => $cssClass,
-                        'params' => $params,
-                        'block_specific_params' => $blockSpecificParams,
+                        'css_class' => $requestAttributes->get('cssClass') ?: false,
+                        'params' => $requestAttributes->get('params') ?: array(),
+                        'block_specific_params' => $requestAttributes->get('blockSpecificParams') ?: array(),
                     ),
                     $response
                 );
             }
         }
 
-        $response->headers->set('X-Location-Id', $layout->contentInfo->mainLocationId);
-
         return $response;
+    }
+
+    protected function processCacheSettings(Request $request, Response $response)
+    {
+        $cacheSettings = $request->attributes->get('cacheSettings');
+        if (!is_array($cacheSettings)) {
+            $cacheSettings = array('sharedMaxAge' => 86400);
+        }
+
+        $public = true;
+
+        if (isset($cacheSettings['sharedMaxAge'])) {
+            $response->setSharedMaxAge($cacheSettings['sharedMaxAge']);
+            if (empty($cacheSettings['sharedMaxAge'])) {
+                $public = false;
+            }
+        } elseif (isset($cacheSettings['maxAge'])) {
+            $response->setMaxAge($cacheSettings['maxAge']);
+            if (empty($cacheSettings['maxAge'])) {
+                $public = false;
+            }
+        } else {
+            $response->setSharedMaxAge(86400);
+        }
+
+        if ($public) {
+            $response->setPublic();
+        }
     }
 }

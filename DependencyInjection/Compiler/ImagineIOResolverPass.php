23ad601f8ccf970bc99fa9f7aa2db1bd2f18da12
Edi Modrić <edi.modric@gmail.com>
2017-04-27 21:05:58 +0200

    Implement compiler pass for overriding IORepositoryResolver

---
+++ DependencyInjection/Compiler/ImagineIOResolverPass.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Netgen\Bundle\MoreBundle\DependencyInjection\Compiler;
+
+use Netgen\Bundle\MoreBundle\Imagine\IORepositoryResolver;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class ImagineIOResolverPass implements CompilerPassInterface
+{
+    /**
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     */
+    public function process(ContainerBuilder $container)
+    {
+        if ($container->has('ezpublish.image_alias.imagine.cache_resolver')) {
+            $container
+                ->findDefinition('ezpublish.image_alias.imagine.cache_resolver')
+                ->setClass(IORepositoryResolver::class);
+        }
+    }
+}

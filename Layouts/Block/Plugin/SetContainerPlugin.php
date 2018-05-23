2d35fd48c7150beb11759154d78da48e322491f7
Goran Martinjak <goran@netgen.hr>
2018-05-23 10:45:13 +0200

    add set container plugin for container sizes

---
+++ Layouts/Block/Plugin/SetContainerPlugin.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Bundle\MoreBundle\Layouts\Block\Plugin;
+
+use Netgen\BlockManager\Block\BlockDefinition\BlockDefinitionHandlerInterface;
+use Netgen\BlockManager\Block\BlockDefinition\Handler\Plugin;
+use Netgen\BlockManager\Parameters\ParameterBuilderInterface;
+use Netgen\BlockManager\Parameters\ParameterType;
+
+class SetContainerPlugin extends Plugin
+{
+    public static function getExtendedHandler()
+    {
+        return [BlockDefinitionHandlerInterface::class];
+    }
+
+    public function buildParameters(ParameterBuilderInterface $builder): void
+    {
+        $designGroup = [self::GROUP_DESIGN];
+
+        $builder->remove('set_container');
+
+        $builder->add(
+            'set_container',
+            ParameterType\Compound\BooleanType::class,
+            [
+                'label' => 'block.plugin.common_params.set_container',
+                'translatable' => false,
+                'groups' => $designGroup,
+            ]
+        );
+
+        $builder->get('set_container')->add(
+            'set_container:size',
+            ParameterType\ChoiceType::class,
+            [
+                'default_value' => 'regular',
+                'label' => 'block.plugin.set_container.size',
+                'translatable' => false,
+                'options' => [
+                    'block.plugin.set_container.regular' => 'regular',
+                    'block.plugin.set_container.narrow' => 'narrow',
+                    'block.plugin.set_container.wide' => 'wide',
+                ],
+                'groups' => $designGroup,
+            ]
+        );
+    }
+}

6637851d585782475512058c5e96900602412939
Mario Blažek <blazek.mario@gmail.com>
2019-04-16 15:26:49 +0200

    Update handler with RequestStack dependency and minor typos

--- bundle/OpenGraph/Handler/SiteImage.php
+++ bundle/OpenGraph/Handler/SiteImage.php
@@ -9,6 +9,8 @@ use eZ\Publish\Core\FieldType\Image\Value as ImageValue;
 use Netgen\Bundle\OpenGraphBundle\Handler\HandlerInterface;
 use Netgen\Bundle\OpenGraphBundle\MetaTag\Item;
 use Netgen\Bundle\SiteBundle\Helper\SiteInfoHelper;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Request;
 
 class SiteImage implements HandlerInterface
 {
@@ -17,7 +19,7 @@ class SiteImage implements HandlerInterface
      *
      * @var string
      */
-    protected $fieldDefinitionIdentifier = 'site_opengraph_image';
+    protected const FIELD_IDENTIFIER = 'site_opengraph_image';
 
     /**
      * @var \Netgen\Bundle\SiteBundle\Helper\SiteInfoHelper
@@ -25,13 +27,20 @@ class SiteImage implements HandlerInterface
     protected $siteInfoHelper;
 
     /**
+     * @var \Symfony\Component\HttpFoundation\RequestStack
+     */
+    private $requestStack;
+
+    /**
      * SiteImage constructor.
      *
      * @param \Netgen\Bundle\SiteBundle\Helper\SiteInfoHelper $siteInfoHelper
+     * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
      */
-    public function __construct(SiteInfoHelper $siteInfoHelper)
+    public function __construct(SiteInfoHelper $siteInfoHelper, RequestStack $requestStack)
     {
         $this->siteInfoHelper = $siteInfoHelper;
+        $this->requestStack = $requestStack;
     }
 
     /**
@@ -42,32 +51,36 @@ class SiteImage implements HandlerInterface
         $siteInfoContent = $this->siteInfoHelper->getSiteInfoContent();
 
         if (
-            $siteInfoContent->hasField($this->fieldDefinitionIdentifier)
-            && $siteInfoContent->getField($this->fieldDefinitionIdentifier)->isEmpty()
-            && $siteInfoContent->getFieldValue($this->fieldDefinitionIdentifier) instanceof ImageValue
+            $siteInfoContent->hasField(self::FIELD_IDENTIFIER)
+            && $siteInfoContent->getField(self::FIELD_IDENTIFIER)->isEmpty()
+            && $siteInfoContent->getFieldValue(self::FIELD_IDENTIFIER) instanceof ImageValue
         ) {
 
             $siteImage = $this->siteInfoHelper
                 ->getSiteInfoContent()
-                ->getField($this->fieldDefinitionIdentifier)
+                ->getField(self::FIELD_IDENTIFIER)
                 ->value
                 ->uri;
 
         } elseif (!empty($params[0])) {
-
             $siteImage = (string) $params[0];
-
         } else {
 
             throw new InvalidArgumentException(
-                sprintf('%s or $params[0]', $this->fieldDefinitionIdentifier),
+                sprintf('%s or $params[0]', self::FIELD_IDENTIFIER),
                 'Either field does not provide valid value or fallback is not properly set up.'
             );
 
         }
 
+        $request = $this->requestStack->getCurrentRequest();
+
+        if (!preg_match('/^https?:\/\//', $siteImage) && $request instanceof Request) {
+            $siteImage = $request->getUriForPath('/' . ltrim($siteImage, '/'));
+        }
+
         return [
-            new Item($tagName, trim($siteImage)),
+            new Item($tagName, $siteImage),
         ];
     }
 }

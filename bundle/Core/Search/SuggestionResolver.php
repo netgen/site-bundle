3e8c09c8641d8f9c868449a70254273b20d7ad9e
Randy Čupić <randy@netgen.hr>
2020-09-04 15:09:01 +0200

    NGSTACK-325 Fix message for exception

--- bundle/Core/Search/SuggestionResolver.php
+++ bundle/Core/Search/SuggestionResolver.php
@@ -13,11 +13,6 @@ class SuggestionResolver
     /**
      * Get suggested search term based on returned spell check suggestions.
      *
-     * @param \eZ\Publish\API\Repository\Values\Content\Query $query
-     * @param \Netgen\EzPlatformSearchExtra\API\Values\Content\Search\Suggestion $suggestion
-     *
-     * @return string
-     *
      * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
      */
     public function getSuggestedSearchTerm(Query $query, Suggestion $suggestion): string
@@ -33,7 +28,7 @@ class SuggestionResolver
         $suggestedTerm = str_replace($search, $replace, $searchTerm);
 
         if ($searchTerm === $suggestedTerm) {
-            throw new NotFoundException('suggestion', 'for search term {'.$searchTerm.'}');
+            throw new NotFoundException('suggestion', sprintf('for search term "%s"', $searchTerm));
         }
 
         return $suggestedTerm;

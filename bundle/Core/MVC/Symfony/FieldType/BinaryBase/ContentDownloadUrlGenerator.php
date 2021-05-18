7253f6bb44b9b22cd436c60c6bb7f580292f022c
Edi Modrić <edi.modric@gmail.com>
2021-05-18 13:48:43 +0200

    Update ContentDownloadUrlGenerator to follow base interface. Yay for semver and BC :/

--- bundle/Core/MVC/Symfony/FieldType/BinaryBase/ContentDownloadUrlGenerator.php
+++ bundle/Core/MVC/Symfony/FieldType/BinaryBase/ContentDownloadUrlGenerator.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 namespace Netgen\Bundle\SiteBundle\Core\MVC\Symfony\FieldType\BinaryBase;
 
 use eZ\Publish\SPI\FieldType\BinaryBase\PathGenerator;
+use eZ\Publish\SPI\FieldType\BinaryBase\RouteAwarePathGenerator;
 use eZ\Publish\SPI\Persistence\Content\Field;
 use eZ\Publish\SPI\Persistence\Content\VersionInfo;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\RouterInterface;
 
 /**
  * Generates the path to the file stored in provided field.
@@ -15,23 +16,37 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
  * Overrides the base generator to allow generating the link with
  * Netgen Site specific route instead of the built in one.
  */
-class ContentDownloadUrlGenerator extends PathGenerator
+class ContentDownloadUrlGenerator extends PathGenerator implements RouteAwarePathGenerator
 {
-    protected UrlGeneratorInterface $urlGenerator;
+    protected RouterInterface $router;
 
-    public function __construct(UrlGeneratorInterface $urlGenerator)
+    protected string $route = 'ngsite_download';
+
+    public function __construct(RouterInterface $router)
     {
-        $this->urlGenerator = $urlGenerator;
+        $this->router = $router;
     }
 
     public function getStoragePathForField(Field $field, VersionInfo $versionInfo): string
     {
-        return $this->urlGenerator->generate(
-            'ngsite_download',
-            [
-                'contentId' => $versionInfo->contentInfo->id,
-                'fieldId' => $field->id,
-            ],
-        );
+        return $this->generate($this->route, $this->getParameters($field, $versionInfo));
+    }
+
+    public function generate(string $route, ?array $parameters = []): string
+    {
+        return $this->router->generate($route, $parameters ?? []);
+    }
+
+    public function getRoute(Field $field, VersionInfo $versionInfo): string
+    {
+        return $this->route;
+    }
+
+    public function getParameters(Field $field, VersionInfo $versionInfo): array
+    {
+        return [
+            'contentId' => $versionInfo->contentInfo->id,
+            'fieldId' => $field->id,
+        ];
     }
 }

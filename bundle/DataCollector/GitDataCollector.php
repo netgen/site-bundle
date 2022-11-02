1c3a7ccab8c6064a50c148ff994355f1806eed0c
Ivan Herak <herak.ivan@gmail.com>
2022-11-02 12:35:05 +0100

    NGGW-89 add basic git branch information to webprofiler

---
+++ bundle/DataCollector/GitDataCollector.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Bundle\SiteBundle\DataCollector;
+
+use SebastianFeldmann\Git\Repository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\DataCollector\DataCollector;
+
+final class GitDataCollector extends DataCollector
+{
+    public function collect(Request $request, Response $response, \Exception $exception = null)
+    {
+        $repository = new Repository();
+
+        $branch = $repository->getInfoOperator()->getCurrentBranch();
+        $lastCommitHash = $repository->getInfoOperator()->getCurrentCommitHash();
+
+        $this->data = [
+            'git_branch' => $branch,
+            'last_commit_hash' => $lastCommitHash,
+        ];
+    }
+
+    public function getGitBranch(): string
+    {
+        return $this->data['git_branch'];
+    }
+
+    public function getLastCommitHash(): string
+    {
+        return $this->data['last_commit_hash'];
+    }
+
+    public function getName()
+    {
+        return 'ngsite.data_collector.git';
+    }
+}

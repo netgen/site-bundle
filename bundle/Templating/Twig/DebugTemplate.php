807c2e82dc0fa92279b37470f5b53b60184e727f
Edi Modrić <edi.modric@gmail.com>
2024-07-26 11:42:07 +0200

    Refactor support for template start and stop markup to use yield

--- bundle/Templating/Twig/DebugTemplate.php
+++ bundle/Templating/Twig/DebugTemplate.php
@@ -10,6 +10,7 @@ use Twig\Template;
 
 use function dirname;
 use function getcwd;
+use function implode;
 use function mb_stripos;
 use function mb_substr;
 use function preg_replace;
@@ -20,53 +21,56 @@ use function trim;
 /**
  * Meant to be used as a Twig base template class.
  *
- * Wraps the display method to:
+ * Wraps the yield method to:
  * - Inject debug info into template to be able to see in the markup which one is used
  */
 class DebugTemplate extends Template
 {
     private Filesystem $fileSystem;
 
-    public function render(array $context): string
+    public function yield(array $context, array $blocks = []): iterable
     {
         $this->fileSystem = $this->fileSystem ?? new Filesystem();
 
-        // Get the template result to be able to insert template name as HTML comments if applicable.
+        // Get parent result to be able to insert template name as HTML comments if applicable.
         // Layout template name will only appear at the end, to avoid potential quirks with old browsers
         // when comments appear before doctype declaration.
-        $templateResult = parent::render($context);
+        $templateResult = implode('', [...parent::yield($context, $blocks)]);
 
         $templateName = trim($this->fileSystem->makePathRelative($this->getSourceContext()->getPath(), dirname((string) getcwd())), '/');
         $isHtmlTemplate = str_ends_with($templateName, 'html.twig');
         $templateName = $isHtmlTemplate ? $templateName . ' (' . $this->getSourceContext()->getName() . ')' : $templateName;
 
-        if (!$isHtmlTemplate) {
-            return $templateResult;
-        }
-
-        // Display start template comment
-        if (str_contains(trim($templateResult), '<!doctype')) {
-            $templateResult = (string) preg_replace(
-                '#(<!doctype[^>]+>)#im',
-                "$1\n<!-- START " . $templateName . ' -->',
-                $templateResult,
-            );
-        } else {
-            $templateResult = "\n<!-- START " . $templateName . " -->\n" . $templateResult;
+        // Display start template comment, if applicable.
+        if ($isHtmlTemplate) {
+            if (str_contains(trim($templateResult), '<!doctype')) {
+                $templateResult = (string) preg_replace(
+                    '#(<!doctype[^>]+>)#im',
+                    "$1\n<!-- START " . $templateName . ' -->',
+                    $templateResult,
+                );
+            } else {
+                yield "\n<!-- START " . $templateName . " -->\n";
+            }
         }
 
         // Display stop template comment after result, if applicable.
-        if (str_contains($templateResult, '</body>')) {
-            $bodyPos = (int) mb_stripos($templateResult, '</body>');
-            // Add layout template name before </body>, to avoid display quirks in some browsers.
-            $templateResult = mb_substr($templateResult, 0, $bodyPos)
-                 . "\n<!-- STOP " . $templateName . " -->\n"
-                 . mb_substr($templateResult, $bodyPos);
-        } else {
-            $templateResult .= "\n<!-- STOP " . $templateName . " -->\n";
-        }
+        if ($isHtmlTemplate) {
+            if (str_contains($templateResult, '</body>')) {
+                $bodyPos = (int) mb_stripos($templateResult, '</body>');
 
-        return $templateResult;
+                // Add layout template name before </body>, to avoid display quirks in some browsers.
+                yield mb_substr($templateResult, 0, $bodyPos)
+                     . "\n<!-- STOP " . $templateName . " -->\n"
+                     . mb_substr($templateResult, $bodyPos);
+            } else {
+                yield $templateResult;
+
+                yield "\n<!-- STOP " . $templateName . " -->\n";
+            }
+        } else {
+            yield $templateResult;
+        }
     }
 
     public function getTemplateName()

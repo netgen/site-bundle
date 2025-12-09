546dcc5cb65dadd5ad85264c0a113c22baa5b02f
Edi Modrić <edi.modric@gmail.com>
2025-12-09 11:00:35 +0100

    No need for ContentDownloadUrlGenerator to extend PathGenerator any more

--- bundle/Core/MVC/Symfony/FieldType/BinaryBase/ContentDownloadUrlGenerator.php
+++ bundle/Core/MVC/Symfony/FieldType/BinaryBase/ContentDownloadUrlGenerator.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Netgen\Bundle\SiteBundle\Core\MVC\Symfony\FieldType\BinaryBase;
 
-use Ibexa\Contracts\Core\FieldType\BinaryBase\PathGenerator;
 use Ibexa\Contracts\Core\FieldType\BinaryBase\RouteAwarePathGenerator;
 use Ibexa\Contracts\Core\Persistence\Content\Field;
 use Ibexa\Contracts\Core\Persistence\Content\VersionInfo;
@@ -16,7 +15,7 @@ use Symfony\Component\Routing\RouterInterface;
  * Overrides the base generator to allow generating the link with
  * Netgen Site specific route instead of the built in one.
  */
-final class ContentDownloadUrlGenerator extends PathGenerator implements RouteAwarePathGenerator
+final class ContentDownloadUrlGenerator implements RouteAwarePathGenerator
 {
     private string $route = 'ngsite_download';
 
